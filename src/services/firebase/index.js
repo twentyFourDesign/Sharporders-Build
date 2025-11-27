293a22c051bd79a71f9caf412f10c3e5d53e5366
Ali <140872322+AliAhmedsh@users.noreply.github.com>
2025-11-27 05:09:48 +0500

    upd

--- src/services/firebase/index.js
+++
@@ -1,7 +0,0 @@
-// Export all Firebase services
-export { firebaseLoadsService } from './loadsService';
-export { firebaseShipmentsService } from './shipmentsService';
-export { firebaseDriversService } from './driversService';
-export { realTimeService } from './realTimeService';
-export { seedSampleData, clearSampleData, sampleData } from './sampleData';
-export { imageUploadService } from './imageUploadService';
